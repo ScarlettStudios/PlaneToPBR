7ec86ff83cc1d8719929cd8f5aff1d10b48e51fe
jshilts <joshuashilts@gmail.com>
2026-02-23 15:43:42 -0500

    refactor: move imports within function scope to prevent importing bpy to tests

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,19 +4,13 @@ bl_info = {
     "category": "Object",
 }
 
-import bpy
-
-from .operators import (
-    OBJECT_OT_import_plane_from_image,
-    menu_func,
-)
-
 def register():
-    print("PLANETOPBR register() CALLED")
+    import bpy
+    from .operators import OBJECT_OT_import_plane_from_image
     bpy.utils.register_class(OBJECT_OT_import_plane_from_image)
     bpy.types.VIEW3D_MT_mesh_add.append(menu_func)
 
 def unregister():
-    print("PLANETOPBR unregister() CALLED")
-    bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
+    import bpy
+    from .operators import OBJECT_OT_import_plane_from_image
     bpy.utils.unregister_class(OBJECT_OT_import_plane_from_image)

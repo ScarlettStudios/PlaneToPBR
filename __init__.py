004930f2a90bfd45ba55c78171c53a8abb885845
jshilts <joshuashilts@gmail.com>
2026-02-27 13:24:03 -0500

    chore: correct import paths for tests

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
-from .scripts import operators, panels, properties
-
 def register():
+    from .scripts import operators, panels, properties
+
     properties.register()
     operators.register()
     panels.register()
 
 def unregister():
+    from .scripts import operators, panels, properties
+
     panels.unregister()
     operators.unregister()
     properties.unregister()

05902900692da880c24dac526cbc3208cb4c7722
jshilts <joshuashilts@gmail.com>
2026-02-27 13:10:08 -0500

    refactor(operators): migrate to Scene-based prompt and remove menu/draw logic

--- scripts/operators.py
+++ scripts/operators.py
@@ -6,6 +6,7 @@ from .utils import import_plane_from_image
 
 class OBJECT_OT_import_plane_from_image(bpy.types.Operator, ImportHelper):
     """Operator to import a plane with a PBR material using an image as reference."""
+
     bl_idname = "object.import_plane_from_image"
     bl_label = "Import Plane from Image"
     bl_options = {'REGISTER', 'UNDO'}
@@ -18,7 +19,6 @@ class OBJECT_OT_import_plane_from_image(bpy.types.Operator, ImportHelper):
 
     def execute(self, context):
         """Execute the operator and import the plane from the diffuse image."""
-        #textures = load_pbr_textures(self.directory)
         prompt = context.scene.planetopbr_prompt
         textures = call_hf_pbr(self.filepath, prompt=prompt)
         import_plane_from_image(textures)
@@ -29,10 +29,9 @@ class OBJECT_OT_import_plane_from_image(bpy.types.Operator, ImportHelper):
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
 
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, "hf_prompt")
 
-def menu_func(self, context):
-    """Add the operator to the 'Add Mesh' menu."""
-    self.layout.operator(OBJECT_OT_import_plane_from_image.bl_idname)
+def register():
+    bpy.utils.register_class(OBJECT_OT_import_plane_from_image)
+
+def unregister():
+    bpy.utils.unregister_class(OBJECT_OT_import_plane_from_image)

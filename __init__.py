a222ead1849a88a3104beec60c2a2bbf1f9e83cc
jshilts <joshuashilts@gmail.com>
2026-02-27 05:27:29 -0500

    feat(ui): add persistent Scene property for HF prompt

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-
-
 bl_info = {
     "name": "planetopbr",
     "blender": (2, 80, 0),
@@ -8,12 +6,23 @@ bl_info = {
 
 def register():
     import bpy
+    from bpy.props import StringProperty
     from .scripts.operators import OBJECT_OT_import_plane_from_image
     from .scripts.operators import menu_func
+    bpy.types.Scene.planetopbr_prompt = StringProperty(
+        name="HF Prompt",
+        description="Describe the material or surface",
+        default=""
+    )
+
     bpy.utils.register_class(OBJECT_OT_import_plane_from_image)
     bpy.types.VIEW3D_MT_mesh_add.append(menu_func)
 
 def unregister():
     import bpy
     from .scripts.operators import OBJECT_OT_import_plane_from_image
+    del bpy.types.Scene.planetopbr_prompt
+
+    # Uncomment in next ticket
+    #bpy.utils.unregister_class(VIEW3D_PT_planetopbr_panel)
     bpy.utils.unregister_class(OBJECT_OT_import_plane_from_image)
